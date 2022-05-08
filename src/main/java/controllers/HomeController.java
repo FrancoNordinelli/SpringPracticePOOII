e095720535fd97fa357ea2a9bc3f53e9ed6ec41d
admin <imnordi@gmail.com>
2022-05-08 15:28:11 -0300

    changes in folders

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,19 +1,22 @@
-package com.unla.SpringPracticePOOII;
+package controllers;
 
+//No funciona sólo con @Controller para la clase
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //Tipo de componente
-@RestController
+@ResponseBody
+
 //Ruta por la que se accede a través de Request
-@RequestMapping("/firstspring")
-public class FirstSpringController {
+@RequestMapping("/")
+public class HomeController {
 
 	//Se accede al método a trávés de una get request
-	@GetMapping("helloworld")
+	@GetMapping("")
 	public String helloWorld() {
-		return "Hello World";
+		return "home/index";
 	}
 }
